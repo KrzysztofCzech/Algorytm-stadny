17a669c258e429d9a0e27d43933c71ee249e1914
Krzysztof <kczech123kczech123@gmail.com>
2022-05-31 10:01:28 +0200

    results update

--- plots.py
+++ plots.py
@@ -10,7 +10,7 @@ def draw_comparision_agents_plot(Agents_list : List[Agent], name :  Str):
     for agent in Agents_list:
         plt.plot(range(len(agent.Island.get_history_soultion())), agent.Island.get_history_soultion())
     alg1 = Agents_list[0].Island.algorithm
-    plt.legend([agent.Island.algorithm.get_name() for agent in Agents_list])
+    plt.legend([agent.name for agent in Agents_list])
     plt.title(f"Problem {alg1.problem.get_name()} with {alg1.problem.number_of_variables} variables")
     plt.savefig(os.path.join('figs', f"{name}.jpg"))
     plt.show()
